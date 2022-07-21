4a1b4bc440a3761ed12aa55064a0e1d9456beacd
discordtehe <79464507+discordtehe@users.noreply.github.com>
2022-07-21 10:46:20 -0400

    Update 2022_7_20.js

--- digworm/2022_7_20.js
+++ digworm/2022_7_20.js
@@ -491,30 +491,30 @@
   1 === window.devicePixelRatio && _0x91abc2(highResEl, false)
   ;(function () {
     for (
-      var _0x794544 = 0,
-        _0xf7917c = ['ms', 'moz', 'webkit', 'o'],
-        _0x8e8ffe = 0;
-      _0x8e8ffe < _0xf7917c.length && !window.requestAnimationFrame;
-      ++_0x8e8ffe
+      var lastTime = 0,
+        vendors = ['ms', 'moz', 'webkit', 'o'],
+        x = 0;
+      x < vendors.length && !window.requestAnimationFrame;
+      ++x
     ) {
       window.requestAnimationFrame =
-        window[_0xf7917c[_0x8e8ffe] + 'RequestAnimationFrame']
+        window[vendors[x] + 'RequestAnimationFrame']
       window.cancelAnimationFrame =
-        window[_0xf7917c[_0x8e8ffe] + 'CancelAnimationFrame'] ||
-        window[_0xf7917c[_0x8e8ffe] + 'CancelRequestAnimationFrame']
+        window[vendors[x] + 'CancelAnimationFrame'] ||
+        window[vendors[x] + 'CancelRequestAnimationFrame']
     }
     window.requestAnimationFrame ||
-      (window.requestAnimationFrame = function (_0x2c2200, _0x5a6930) {
-        var _0x4cf495 = new Date().getTime(),
-          _0x183a2d = Math.max(0, 16 - (_0x4cf495 - _0x794544)),
-          _0x2eb8eb = window.setTimeout(function () {
-            _0x2c2200(_0x4cf495 + _0x183a2d)
-          }, _0x183a2d)
-        return (_0x794544 = _0x4cf495 + _0x183a2d), _0x2eb8eb
+      (window.requestAnimationFrame = function (callback, element) {
+        var currTime = new Date().getTime(),
+          timeToCall = Math.max(0, 16 - (currTime - lastTime)),
+          id = window.setTimeout(function () {
+            callback(currTime + timeToCall)
+          }, timeToCall)
+        return (lastTime = currTime + timeToCall), id
       })
     window.cancelAnimationFrame ||
-      (window.cancelAnimationFrame = function (_0x32a71d) {
-        clearTimeout(_0x32a71d)
+      (window.cancelAnimationFrame = function (id) {
+        clearTimeout(id)
       })
   })()
   const killerEl = document.querySelector('.killer'),
